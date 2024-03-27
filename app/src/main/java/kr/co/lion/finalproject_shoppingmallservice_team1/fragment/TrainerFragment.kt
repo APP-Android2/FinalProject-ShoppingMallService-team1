6ec233081bf8c9c7bdb1932f6bae8c5899c8dd3e
gns402 <gns402@naver.com>
2024-03-27 18:08:51 +0900

    [Feat] fragment_trainer.xml 화면 구현

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/fragment/TrainerFragment.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/fragment/TrainerFragment.kt
@@ -1,17 +1,40 @@
 package kr.co.lion.finalproject_shoppingmallservice_team1.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import kr.co.lion.finalproject_shoppingmallservice_team1.NavigationActivity
 import kr.co.lion.finalproject_shoppingmallservice_team1.R
+import kr.co.lion.finalproject_shoppingmallservice_team1.databinding.FragmentTrainerBinding
+
+
 class TrainerFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trainer, container, false)
+
+    lateinit var fragmentTrainerBinding: FragmentTrainerBinding
+    lateinit var navigationActivity: NavigationActivity
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        fragmentTrainerBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_trainer, container, false)
+
+        navigationActivity = activity as NavigationActivity
+
+
+        settingToolbarTrainer()
+
+        return fragmentTrainerBinding.root
+    }
+
+    // 툴바 설정
+    fun settingToolbarTrainer(){
+        fragmentTrainerBinding.apply {
+            toolbarTrainer.apply {
+                //title = "트레이너"
+                inflateMenu(R.menu.menu_trainer)
+            }
+        }
     }
 }
