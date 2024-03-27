d9a5d038841d68d323aba56f2dea5abb261d00f2
iamByeongJin <leej4528@naver.com>
2024-03-27 14:26:32 +0900

    [Feat] Splash 화면

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/MainActivity.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/MainActivity.kt
@@ -1,7 +1,10 @@
 package kr.co.lion.finalproject_shoppingmallservice_team1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import kr.co.lion.finalproject_shoppingmallservice_team1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Splash 화면
+        installSplashScreen()
+        SystemClock.sleep(700)
+
         activityMaingBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMaingBinding.root)
+
+        // 테스트 코드
+        activityMaingBinding.button.setOnClickListener {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
