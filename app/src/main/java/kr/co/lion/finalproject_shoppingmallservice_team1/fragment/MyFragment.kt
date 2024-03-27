e53e676a71c6b84c213ff89535484afb9f5c1992
YoungJuns0928 <eric040928@naver.com>
2024-03-27 18:01:55 +0900

    [Feat] My페이지 화면 구성

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/fragment/MyFragment.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/fragment/MyFragment.kt
@@ -5,14 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.lion.finalproject_shoppingmallservice_team1.NavigationActivity
 import kr.co.lion.finalproject_shoppingmallservice_team1.R
+import kr.co.lion.finalproject_shoppingmallservice_team1.databinding.FragmentMyBinding
 
 class MyFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+
+    lateinit var fragmentMyBinding: FragmentMyBinding
+    lateinit var navigationActivity: NavigationActivity
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my, container, false)
+        fragmentMyBinding =DataBindingUtil.inflate(inflater, R.layout.fragment_my, container, false)
+
+        navigationActivity = activity as NavigationActivity
+
+        settingToolbarMy()
+
+        return fragmentMyBinding.root
+    }
+
+    // Toolbar 설정
+    fun settingToolbarMy(){
+        fragmentMyBinding.apply {
+            toolbarMy.apply {
+                // 타이틀
+                title = "마이페이지"
+                // 메뉴
+                inflateMenu(R.menu.menu_my)
+            }
+        }
     }
 }
