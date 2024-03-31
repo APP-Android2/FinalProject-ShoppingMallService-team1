f1d44ef30874d9ff59430b12cc4caf4af8dcbc07
iamByeongJin <leej4528@naver.com>
2024-04-01 01:54:29 +0900

    [Fix] SplashScreen [Feat] OnBoardingScreen

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/MainActivity.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/MainActivity.kt
@@ -1,142 +1,46 @@
 package kr.co.lion.finalproject_shoppingmallservice_team1
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import com.google.android.material.transition.MaterialSharedAxis
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import kr.co.lion.finalproject_shoppingmallservice_team1.databinding.ActivityMainBinding
-import kr.co.lion.finalproject_shoppingmallservice_team1.fragment.CenterFragment
-import kr.co.lion.finalproject_shoppingmallservice_team1.fragment.CommunityFragment
-import kr.co.lion.finalproject_shoppingmallservice_team1.fragment.HomeFragment
-import kr.co.lion.finalproject_shoppingmallservice_team1.fragment.MyFragment
-import kr.co.lion.finalproject_shoppingmallservice_team1.fragment.TrainerFragment
+import kr.co.lion.finalproject_shoppingmallservice_team1.fragment.OnBoarding1Fragment
+import kr.co.lion.finalproject_shoppingmallservice_team1.fragment.OnBoarding2Fragment
+import kr.co.lion.finalproject_shoppingmallservice_team1.fragment.OnBoarding3Fragment
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var activityMaingBinding: ActivityMainBinding
-
-    // 프래그먼트의 주소값을 담을 프로퍼티
-    var oldFragment: Fragment? = null
-    var newFragment: Fragment? = null
+    private lateinit var activityMainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Splash 화면
         installSplashScreen()
-        SystemClock.sleep(700)
 
-        activityMaingBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(activityMaingBinding.root)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
 
-        // 테스트 코드
-        // 네비게이션 activity로 가기
-        activityMaingBinding.button.setOnClickListener {
-            val intent = Intent(this@MainActivity, NavigationActivity::class.java)
-            startActivity(intent)
-        }
+        activityMainBinding.mainViewPager2.adapter = ViewPageAdapter(this@MainActivity)
+        activityMainBinding.mainCircleIndicator.setViewPager(activityMainBinding.mainViewPager2)
     }
+}
 
-    // 지정한 Fragment를 보여주는 메서드
-    // name : 프래그먼트 이름
-    // addToBackStack : BackStack에 포함 시킬 것인지
-    // isAnimate : 애니메이션을 보여줄 것인지
-    // data : 새로운 프래그먼트에 전달할 값이 담겨져 있는 Bundle 객체
-    fun replaceFragment(name:MAIN_FRAGMENT_NAME, addToBackStack:Boolean, isAnimate:Boolean, data:Bundle?){
+class ViewPageAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
 
-        SystemClock.sleep(200)
+    // 페이지 갯수 설정
+    override fun getItemCount(): Int = 3
 
-        // Fragment를 교체할 수 있는 객체를 추출한다.
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-
-        // oldFragment에 newFragment가 가지고 있는 Fragment 객체를 담아준다.
-        if(newFragment != null){
-            oldFragment = newFragment
+    // 불러올 Fragment 정의
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> OnBoarding1Fragment()
+            1 -> OnBoarding2Fragment()
+            2 -> OnBoarding3Fragment()
+            else -> throw IllegalArgumentException("Invalid position $position")
         }
-
-        // 이름으로 분기한다.
-        // Fragment의 객체를 생성하여 변수에 담아준다.
-        when(name){
-
-            // 권한 화면
-            MAIN_FRAGMENT_NAME.PERMISSION_FRAGMENT -> {
-            }
-
-            // 회원 설명 화면
-            MAIN_FRAGMENT_NAME.MEMBER_EXPLANATION_FRAGMENT -> {
-                CenterFragment()
-            }
-
-        }
-
-        // 새로운 Fragment에 전달할 객체가 있다면 arguments 프로퍼티에 넣어준다.
-        if(data != null){
-            newFragment?.arguments = data
-        }
-
-        if(newFragment != null){
-
-            // 애니메이션 설정
-            if(isAnimate == true){
-                // oldFragment -> newFragment
-                // oldFragment : exitTransition
-                // newFragment : enterTransition
-
-                // newFragment -> oldFragment
-                // oldFragment : reenterTransition
-                // newFragment : returnTransition
-
-                // MaterialSharedAxis : 좌우, 위아래, 공중 바닥 사이로 이동하는 애니메이션 효과
-                // X - 좌우
-                // Y - 위아래
-                // Z - 공중 바닥
-                // 두 번째 매개변수 : 새로운 화면이 나타나는 것인지 여부를 설정해준다.
-                // true : 새로운 화면이 나타나는 애니메이션이 동작한다.
-                // false : 이전으로 되돌아가는 애니메이션이 동작한다.
-
-                if(oldFragment != null){
-                    // old에서 new가 새롭게 보여질 때 old의 애니메이션
-                    oldFragment?.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
-                    // new에서 old로 되돌아갈때 old의 애니메이션
-                    oldFragment?.reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
-
-                    oldFragment?.enterTransition = null
-                    oldFragment?.returnTransition = null
-                }
-
-                // old에서 new가 새롭게 보여질 때 new의 애니메이션
-                newFragment?.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
-                // new에서 old로 되돌아갈때 new의 애니메이션
-                newFragment?.returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
-
-                newFragment?.exitTransition = null
-                newFragment?.reenterTransition = null
-            }
-
-            // Fragment를 교체한다.(이전 Fragment가 없으면 새롭게 추가하는 역할을 수행한다)
-            // 첫 번째 매개 변수 : Fragment를 배치할 FragmentContainerView의 ID
-            // 두 번째 매개 변수 : 보여주고하는 Fragment 객체를
-            fragmentTransaction.replace(R.id.Navigation_fragmentContainerView, newFragment!!)
-
-            // addToBackStack 변수의 값이 true면 새롭게 보여질 Fragment를 BackStack에 포함시켜 준다.
-            if(addToBackStack == true){
-                // BackStack 포함 시킬때 이름을 지정해주면 원하는 Fragment를 BackStack에서 제거할 수 있다.
-                fragmentTransaction.addToBackStack(name.str)
-            }
-            // Fragment 교체를 확정한다.
-            fragmentTransaction.commit()
-        }
-    }
-
-    // BackStack에서 Fragment를 제거한다.
-    fun removeFragment(name:Navigation_FRAGMENT_NAME){
-        SystemClock.sleep(200)
-
-        // 지정한 이름으로 있는 Fragment를 BackStack에서 제거한다.
-        supportFragmentManager.popBackStack(name.str, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 }
