7fc0d609ae884d5f3a23b8a2456ce3ec27ae55c7
YeongJun <eric040928@naver.com>
2024-04-02 14:09:20 +0900

    [Feat] MySettingFragment 설정 화면 구성

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/fragment/MySettingFragment.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/fragment/MySettingFragment.kt
@@ -5,15 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.lion.finalproject_shoppingmallservice_team1.NavigationActivity
 import kr.co.lion.finalproject_shoppingmallservice_team1.R
+import kr.co.lion.finalproject_shoppingmallservice_team1.databinding.FragmentMySettingBinding
 
 class MySettingFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    lateinit var fragmentMySettingBinding: FragmentMySettingBinding
+    lateinit var navigationActivity: NavigationActivity
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_setting, container, false)
+        fragmentMySettingBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_setting, container, false)
+
+        navigationActivity = activity as NavigationActivity
+
+        settingToolbar()
+
+        return fragmentMySettingBinding.root
+    }
+
+    // Toolbar 설정
+    fun settingToolbar(){
+        fragmentMySettingBinding.apply {
+            toolbarMySetting.apply {
+                // 타이틀
+                title = "설정"
+            }
+        }
     }
 }
