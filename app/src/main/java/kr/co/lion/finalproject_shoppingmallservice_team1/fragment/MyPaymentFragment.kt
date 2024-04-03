fcf277ad9251fc5d0901e91a32fdf1161cc55156
YeongJun <eric040928@naver.com>
2024-04-03 16:12:56 +0900

    [Feat] MyPaymentFragment 화면 구성

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/fragment/MyPaymentFragment.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/fragment/MyPaymentFragment.kt
@@ -1,60 +1,97 @@
 package kr.co.lion.finalproject_shoppingmallservice_team1.fragment
 
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.lion.finalproject_shoppingmallservice_team1.NavigationActivity
 import kr.co.lion.finalproject_shoppingmallservice_team1.R
+import kr.co.lion.finalproject_shoppingmallservice_team1.databinding.FragmentMyPaymentBinding
+import kr.co.lion.finalproject_shoppingmallservice_team1.databinding.RowMyPaymentBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPaymentFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyPaymentFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    lateinit var fragmentMyPaymentBinding: FragmentMyPaymentBinding
+    lateinit var navigationActivity: NavigationActivity
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        fragmentMyPaymentBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_payment, container, false)
+        navigationActivity = activity as NavigationActivity
+
+        settingToolbar()
+        settingRecyclerViewMyPayment()
+
+        return fragmentMyPaymentBinding.root
+    }
+
+    // Toolbar 설정
+    fun settingToolbar(){
+        fragmentMyPaymentBinding.apply {
+            toolbarMyPayment.apply {
+                // 뒤로가기
+                setNavigationIcon(R.drawable.arrow_back)
+                setNavigationOnClickListener {
+                    backProcess()
+                }
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_payment, container, false)
+    // 뒤로가기 처리
+    fun backProcess(){
+        SystemClock.sleep(200)
+        parentFragmentManager.popBackStack()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPaymentFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPaymentFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    // RecyclerView 설정
+    fun settingRecyclerViewMyPayment(){
+        fragmentMyPaymentBinding.apply {
+            recyclerViewMyPayment.apply {
+                // 어댑터
+                adapter = MyPaymentRecyclerViewAdapter()
+                // 레이아웃 매니저
+                layoutManager = LinearLayoutManager(navigationActivity)
             }
+        }
+    }
+
+    // RecyclerView 의 어댑터
+    inner class MyPaymentRecyclerViewAdapter : RecyclerView.Adapter<MyPaymentRecyclerViewAdapter.MyPaymentViewHolder>() {
+        // ViewHolder
+        inner class MyPaymentViewHolder(rowMyPaymentBinding: RowMyPaymentBinding) : RecyclerView.ViewHolder(rowMyPaymentBinding.root){
+            val rowMyPaymentBinding: RowMyPaymentBinding
+
+            init {
+                this.rowMyPaymentBinding = rowMyPaymentBinding
+
+                this.rowMyPaymentBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                )
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPaymentViewHolder {
+            val rowMyPaymentBinding = RowMyPaymentBinding.inflate(layoutInflater)
+            val myPaymentViewHolder = MyPaymentViewHolder(rowMyPaymentBinding)
+
+            return myPaymentViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 5
+        }
+
+        override fun onBindViewHolder(holder: MyPaymentViewHolder, position: Int) {
+            holder.rowMyPaymentBinding.tvRowMyPaymentProductName.text = "제품명 $position"
+            holder.rowMyPaymentBinding.tvRowMyPaymentCenterName.text = "운동 센터 이름 $position"
+            holder.rowMyPaymentBinding.tvRowMyPaymentDay.text = "2024-04-03"
+        }
     }
 }
