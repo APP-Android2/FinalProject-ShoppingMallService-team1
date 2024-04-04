61932063ea4af340f1c58b5e61f9074668b427cd
YeongJun <eric040928@naver.com>
2024-04-04 14:55:44 +0900

    [Fead] MyReview 에 Tab2 프래그먼트 화면 구성 및 연결

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/fragment/MyReviewTab2Fragment.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/fragment/MyReviewTab2Fragment.kt
@@ -5,56 +5,73 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.lion.finalproject_shoppingmallservice_team1.NavigationActivity
 import kr.co.lion.finalproject_shoppingmallservice_team1.R
+import kr.co.lion.finalproject_shoppingmallservice_team1.databinding.FragmentMyReviewTab2Binding
+import kr.co.lion.finalproject_shoppingmallservice_team1.databinding.RowMyReviewTab2Binding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyReviewTab2Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyReviewTab2Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    lateinit var fragmentMyReviewTab2Binding: FragmentMyReviewTab2Binding
+    lateinit var navigationActivity: NavigationActivity
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        fragmentMyReviewTab2Binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_review_tab2, container, false)
+        navigationActivity = activity as NavigationActivity
+
+        settingRecyclerViewMyReviewTab2()
+
+        return fragmentMyReviewTab2Binding.root
+    }
+
+    // RecyclerView 구성
+    fun settingRecyclerViewMyReviewTab2(){
+        fragmentMyReviewTab2Binding.apply {
+            recyclerViewMyReviewTab2.apply {
+                // 어댑터
+                adapter = MyReviewTab2RecyclerViewAdapter()
+                // 레이아웃 매니저
+                layoutManager = LinearLayoutManager(navigationActivity)
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_review_tab2, container, false)
-    }
+    // RecyclerView 의 어댑터
+    inner class MyReviewTab2RecyclerViewAdapter : RecyclerView.Adapter<MyReviewTab2RecyclerViewAdapter.MyReviewTab2ViewHolder>() {
+        // ViewHolder
+        inner class MyReviewTab2ViewHolder(rowMyReviewTab2Binding: RowMyReviewTab2Binding) : RecyclerView.ViewHolder(rowMyReviewTab2Binding.root){
+            val rowMyReviewTab2Binding: RowMyReviewTab2Binding
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyReviewTab2Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyReviewTab2Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+            init {
+                this.rowMyReviewTab2Binding = rowMyReviewTab2Binding
+
+                this.rowMyReviewTab2Binding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                )
             }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyReviewTab2ViewHolder {
+            val rowMyReviewTab2Binding = RowMyReviewTab2Binding.inflate(layoutInflater)
+            val myReviewTab2ViewHolder = MyReviewTab2ViewHolder(rowMyReviewTab2Binding)
+
+            return myReviewTab2ViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 5
+        }
+
+        override fun onBindViewHolder(holder: MyReviewTab2ViewHolder, position: Int) {
+            holder.rowMyReviewTab2Binding.tvRowMyReviewTab2Name.text = "고길동"
+            holder.rowMyReviewTab2Binding.tvRowMyReviewTab2Date.text = "2000-04-04"
+            holder.rowMyReviewTab2Binding.tvRowMyReviewTrainerName.text = "트레이너명 $position"
+            holder.rowMyReviewTab2Binding.tvRowMyReviewTab2Contents.text = "우리쌤 체고 $position"
+        }
     }
 }
