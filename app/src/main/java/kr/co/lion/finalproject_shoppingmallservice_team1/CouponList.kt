2e3e822fba3a819cb50c10c26fd3ebaba95caf31
mimo626 <yessuk5499@gmail.com>
2024-04-11 15:14:38 +0900

    [Feature] 구글 로그인, 로그아웃, 회원탈퇴 구현

---
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/CouponList.kt
@@ -0,0 +1,14 @@
+package kr.co.lion.finalproject_shoppingmallservice_team1
+
+// 쿠폰 목록
+class CouponList {
+    // 전체 쿠폰 목록
+    var couponEntireList : MutableList<Coupon> = mutableListOf()
+
+    // 보유한 쿠폰 목록
+    var couponOwnList : MutableList<Coupon> = mutableListOf()
+
+    // 다운로드 가능한 쿠폰 목록
+    var couponDownList : MutableList<Coupon> = mutableListOf()
+
+}
