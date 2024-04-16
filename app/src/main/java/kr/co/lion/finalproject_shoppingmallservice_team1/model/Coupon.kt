3657be1beab1ac727479f814ed719d554e7f7597
mimo626 <yessuk5499@gmail.com>
2024-04-16 09:53:12 +0900

    [Delete] 다이얼로그 파일 삭제 / 쿠폰 모델 변수명 수정

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/model/Coupon.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/model/Coupon.kt
@@ -9,8 +9,8 @@ class Coupon {
     var discountRate:Int = 0 // 쿠폰의 할인율
 
     var startDate:String = "" // 쿠폰을 다운받을 수 있는 첫 시작 날짜
-    var lastDate:String = "" // 쿠폰 만료일
-    var remainPeriod:Int = 0 // 쿠폰의 남은 기간
+    var endDate:String = "" // 쿠폰 만료일
+    var remainingPeriod:Int = 0 // 쿠폰의 남은 기간
 
-    var stateCheck:Boolean = true // 쿠폰 사용 가능 여부(true: 사용 가능 / false: 사용 불가)
+    var availability:Boolean = true // 쿠폰 사용 가능 여부(true: 사용 가능 / false: 사용 불가)
 }
