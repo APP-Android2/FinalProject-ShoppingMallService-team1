8a7dc95360769d9b5a6f15e5faad8c520415951f
YeongJun <eric040928@naver.com>
2024-04-16 13:39:28 +0900

    [Fix] Center 모델 수정

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/model/Center.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/model/Center.kt
@@ -5,7 +5,7 @@ interface Center {
     var centerId: String    // 운동 센터 ID
     var centerName: String    // 운동 센터 이름
     var centerLocation: String    // 운동 센터 위치
-    var centerImageUrl: MutableList<Any>  // 운동 센터 사진
+    var centerImage: MutableList<Any>?  // 운동 센터 사진
 }
 
 // 운동 센터 정보탭
@@ -13,19 +13,22 @@ class CenterInfo : Center {
 
     override var centerId: String = ""  // 운동 센터 ID
     override var centerName: String = ""    // 운동 센터 이름
-    override var centerLocation: String = ""    // 운동 센터 주소
-    override var centerImageUrl: MutableList<Any> = mutableListOf()    // 운동 센터 사진
+    override var centerLocation: String = ""    // 운동 센터 위치
+    override var centerImage: MutableList<Any>? = mutableListOf()    // 운동 센터 사진
 
-    var notification: String = ""   // 공지사항
-    var introduction: String = ""   // 운동 센터 소개말
-    var trainerImage: MutableList<Any> = mutableListOf()    // 트레이너 사진
-    var trainerInfo: MutableList<Any> = mutableListOf()    // 트레이너 간단한 소개
+    var notification: String? = ""   // 공지사항
+    var introduction: String? = ""   // 운동 센터 소개말
+    var trainerImage: MutableList<Any>? = mutableListOf()    // 트레이너 사진
+    var trainerInfo: MutableList<Any>? = mutableListOf()    // 트레이너 간단한 소개
     var openTime: String = ""   // 영업시간 (오픈)
     var closeTime: String = ""  // 영업시간 (마감)
-    var freeService: MutableList<Any> = mutableListOf()   // 부가 서비스 (무료)
-    var paidService: MutableList<Any> = mutableListOf()   // 부가 서비스 (유료)
-    var convenience: MutableList<Any> = mutableListOf()    // 편의시설
-    var sns: MutableList<Any> = mutableListOf()    // SNS 바로가기
+    var freeService: MutableList<Any>? = mutableListOf()   // 부가 서비스 (무료)
+    var paidService: MutableList<Any>? = mutableListOf()   // 부가 서비스 (유료)
+    var convenience: MutableList<Any>? = mutableListOf()    // 편의시설
+    var sns: MutableList<Any>? = mutableListOf()    // SNS 바로가기
+
+    var reviewCount: Int = 0 // 작성된 리뷰의 총 개수
+    var reviewAvg: Double = 0.0 // 리뷰 평균 별점
 }
 
 // 운동 센터 상담탭
@@ -33,13 +36,13 @@ class CenterConsulting : Center {
     override var centerId: String = ""  // 운동 센터 ID
     override var centerName: String = ""  // 운동 센터 이름
     override var centerLocation: String = ""  // 운동 센터 위치
-    override var centerImageUrl: MutableList<Any> = mutableListOf()   // 운동 센터 사진
+    override var centerImage: MutableList<Any>? = mutableListOf()   // 운동 센터 사진
 
     // 방문 상담
     var userName: String = ""   // 이름
     var visitTime: String = ""  // 방문 신청 시간
     var visitReason: String = "" // 운동 목적
-    var other: String = ""  // 기타
+    var other: String? = ""  // 기타 사항
 
     // 채팅 상담
     var chatTrainerName: String = ""    // 트레이너 이름
