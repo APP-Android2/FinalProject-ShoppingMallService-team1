6febe942692d00f432cc3e6c6944b5f86bfc260a
mimo626 <yessuk5499@gmail.com>
2024-04-18 10:13:41 +0900

    [Feature] 방문상담 모델 구현 중

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/model/VisitConsulting.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/model/VisitConsulting.kt
@@ -6,11 +6,11 @@ data class VisitConsulting (
     var visitConsultingId:String = "", // 상담 신청 내역 아이디
 
     var centerId:String = "", // 운동센터 아이디
-    var trainerId:String = "", // 트레이너 아이디
+    var trainerId:String? = "", // 트레이너 아이디
 
     var name:String = "", // 이름
-    var applicationTime:String = "", // 희망 신청 시간
     var exercisePurpose:String = "", // 운동 목적
+    var applicationTime:String = "", // 희망 신청 시간
     var etcContent:String? = "", // 기타
 
     var stateCheck:Boolean = true // 신청 상태(true-상담 예정 / false-상담 종료)
