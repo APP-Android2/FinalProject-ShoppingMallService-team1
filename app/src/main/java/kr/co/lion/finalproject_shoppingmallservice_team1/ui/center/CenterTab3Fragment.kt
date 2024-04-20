9b264a80ab58047827324a07987cc6251bb93051
YeongJun <eric040928@naver.com>
2024-04-20 21:38:10 +0900

    [Feat] 항목 클릭 시 ReadCenterActivity 실행

--- app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/ui/center/CenterTab3Fragment.kt
+++ app/src/main/java/kr/co/lion/finalproject_shoppingmallservice_team1/ui/center/CenterTab3Fragment.kt
@@ -62,6 +62,13 @@ class CenterTab3Fragment : Fragment() {
                     isImageClick = !isImageClick
                     updateImageButton()
                 }
+
+                // 게시글 클릭 설정
+                this.rowCenterTab3Binding.apply {
+                    itemCenterTab3Post.setOnClickListener {
+                        navigationActivity.readCenterRequest()
+                    }
+                }
             }
 
             // 찜 버튼 클릭에 따른 이미지 변경
